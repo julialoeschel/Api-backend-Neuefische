31b01aa2b364b02d80f7e9f0327ad7336af0ddb2
Julia Loeschel <juloeschel@gmail.com>
2021-10-26 15:20:43 +0200

    delete in array

--- src/server.ts
+++ src/server.ts
@@ -2,20 +2,37 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+const users = ['Paul', 'Samet', 'Julia', 'Muhamed', 'Patrick'];
 
 app.get('/api/users/:name', (request, response) => {
-  response.send(request.params);
+  const isNameKnown = users.includes(request.params.name);
+  if (isNameKnown) {
+    response.send(request.params.name);
+  } else {
+    response.status(404).send('Blabla');
+  }
 });
 
-app.get('/api/users', (_request, response) => {
-  const users = ['Paul', 'Samet', 'Julia'];
-  response.send(users);
-});
+app.delete('/api/users/:name', (request, response) => {
+  const isKnown = request.params.name;
+  if (users.includes(isKnown)) {
+    const IndexOfName = users.findIndex((name) => name === isKnown);
+    response.send(`${IndexOfName}`);
+    users.splice(IndexOfName, 1);
+    response.send(users);
+  } else {
+    response.status(404).send('User not found');
+  }
 
-app.get('/', (_request, response) => {
-  response.send('Hallo Welt!');
-});
+  app.get('/api/users', (_request, response) => {
+    response.send(users);
+  });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  app.get('/', (_request, response) => {
+    response.send('Hallo Welt!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
 });
